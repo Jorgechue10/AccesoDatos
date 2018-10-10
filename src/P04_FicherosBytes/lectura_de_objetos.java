5166a6d1f46f6932568873ceeef9a380c7917097
Jorge Chueca <jorgechue10@gmail.com>
2018-10-10 18:44:21 +0200

    Lectura de objetos

--- src/P04_FicherosBytes/lectura_de_objetos.java
+++ src/P04_FicherosBytes/lectura_de_objetos.java
@@ -1,11 +1,29 @@
 package P04_FicherosBytes;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class lectura_de_objetos {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
 		// TODO Auto-generated method stub
-
+		File f = new File("D:\\eclipse-workspace\\AD_01_Fciheros\\src\\P04_FicherosBytes\\personas.dat");
+		
+		FileInputStream filein = new FileInputStream(f);
+		ObjectInputStream objectin = new ObjectInputStream(filein);
+		
+		Persona p;
+		
+		try {
+			while(true){
+					p=(Persona)objectin.readObject();
+					System.out.println("Nombre: "+p.getNombre()+", Edad: "+p.getEdad());
+			}
+		}catch (EOFException eo) {
+			System.out.println("");
+		}		
 	}
 }
