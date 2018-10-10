166e83179f6c0936e6013fec1ff4297c49355a03
Jorge Chueca <jorgechue10@gmail.com>
2018-10-10 18:54:35 +0200

    Ejercicio en que contiene un ejemplo de Scanner

---
+++ src/P05_Utilidades/Ejercicio_Scanner.java
@@ -0,0 +1,20 @@
+package P05_Utilidades;
+
+import java.util.Scanner;
+
+public class Ejercicio_Scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		
+		System.out.println("Introduce dos numeros enteros");
+		int x = teclado.nextInt();
+		int y = teclado.nextInt();
+		int suma;
+		
+		suma = x+y;
+		System.out.println("El resultado es: "+suma);
+		teclado.close();		
+	}
+}
