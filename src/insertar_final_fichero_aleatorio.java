01a61c866c12e6456a5e95a99fb3daa4b3c30424
Jorge Chueca <jorgechue10@gmail.com>
2018-10-17 19:35:00 +0200

    Ejemplo de insertar al final de un fichero

---
+++ src/insertar_final_fichero_aleatorio.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class insertar_final_fichero_aleatorio {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		File f = new File("D:\\eclipse-workspace\\AD_01_Fciheros\\src\\P04_FicherosBytes\\Ficheros\\escritura_fichero_aleatorio.dat");
+		
+		RandomAccessFile raf = new RandomAccessFile (f, "rw");
+		
+		int id = 11;
+		String apellidos = "García";
+		int dpto = 12;
+		double salario = 1000;
+		
+		long posicion = raf.length();		
+		raf.seek(posicion);
+		
+		raf.writeInt(id);//número de empleado
+		
+		StringBuffer sb = null;
+		sb= new StringBuffer(apellidos);
+		sb.setLength(10);
+		raf.writeChars(sb.toString());
+		
+		raf.writeInt(dpto);
+		raf.writeDouble(salario);	
+		
+		raf.close();
+	}
+}
