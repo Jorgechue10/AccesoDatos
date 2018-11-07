c69ba082b1910ac4323c67c3c97ad17eb93302db
Jorge Chueca <jorgechue10@gmail.com>
2018-11-07 18:46:20 +0100

    Hago un convertidor para pasar el xml a html a través de la plantilla

---
+++ src/P05_Ficheros_XML/EJ_4_Convertir_XML_HTML.java
@@ -0,0 +1,33 @@
+/*Para convertir de XML a HTML, primero vamos a tener que crear un fichero xsl 
+ * (en este caso lo voy a crear en la carpeta ficheros).
+ * 
+ * En este ejercicio vamos a convertir el archivo canciones.xml a un html
+ * mediante la plantilla cancionesPlantilla.xsl*/
+
+package P05_Ficheros_XML;
+
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import java.io.*;
+
+public class EJ_4_Convertir_XML_HTML {
+	public static void main(String argv[]) throws IOException{ 
+		String hojaEstilo = "src\\P05_Ficheros_XML\\Ficheros\\cancionesPlantilla.xsl";
+		String datosAlumnos = "src\\P05_Ficheros_XML\\Ficheros\\canciones.xml";
+		File pagHTML = new File("src\\P05_Ficheros_XML\\Ficheros\\canciones.html");
+		FileOutputStream os = new FileOutputStream(pagHTML); //crear fichero HTML
+  
+		Source estilos =new StreamSource(hojaEstilo);//fuente XSL
+		Source datos =new StreamSource(datosAlumnos);//fuente XML
+		Result result = new StreamResult(os);        //resultado de la transformación
+  
+		try{     
+			Transformer transformer =  
+               TransformerFactory.newInstance().newTransformer(estilos);   
+			transformer.transform(datos, result);	//obtiene el HTML
+		}
+		catch(Exception e){System.err.println("Error: "+e);}
+  
+		os.close();  //cerrar fichero 		
+		}
+}
