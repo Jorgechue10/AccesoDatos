c81061a4cc73cbaefac91032c4e625919f9762ec
Jorge Chueca <jorgechue10@gmail.com>
2018-11-21 19:14:58 +0100

    Ejemplo procedimiento utilizando subida_sal_jorge

---
+++ Unidad_02_Conectores/P01_Conectores/EJ_02_Procedimiento_subida_sal.java
@@ -0,0 +1,38 @@
+package P01_Conectores;
+
+import java.sql.*;
+
+public class EJ_02_Procedimiento_subida_sal {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el driver
+			 Connection conexion = DriverManager.getConnection(
+     				"jdbc:mysql://www.db4free.net:3306/tubalcain",
+     				"tubalcain_dam",
+     				"12345678"
+     				); 
+
+			// recuperar parametros de main
+			String dep = "0";   //"10"; // departamento
+			String subida = "1000";//"1000"; // subida
+			
+			// construir orden DE LLAMADA
+			String sql = "{ call subida_sal_jorge (?, ?) } ";
+
+			// Preparamos la llamada
+			CallableStatement llamada = conexion.prepareCall(sql);
+			// Damos valor a los argumentos
+			llamada.setInt(1, Integer.parseInt(dep)); // primer argumento-dep
+			llamada.setFloat(2, Float.parseFloat(subida)); // segundo arg
+															
+			llamada.executeUpdate(); // ejecutar el procedimiento
+			System.out.println("Subida realizada....");
+			llamada.close();
+			conexion.close();
+		} catch (ClassNotFoundException cn) {
+			cn.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
