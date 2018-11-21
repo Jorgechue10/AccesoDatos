6966226332cf85114a8b3ae4310fe040d551e7e8
Jorge Chueca <jorgechue10@gmail.com>
2018-11-21 19:34:48 +0100

    Ejemplo funcion utilizando la funcion call_nombre_jorge

---
+++ Unidad_02_Conectores/P01_Conectores/EJ_03_Funcion_nombre_dept.java
@@ -0,0 +1,49 @@
+package P01_Conectores;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class EJ_03_Funcion_nombre_dept {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el driver
+			 Connection conexion = DriverManager.getConnection(
+     				"jdbc:mysql://www.db4free.net:3306/tubalcain",
+     				"tubalcain_dam",
+     				"12345678"
+     				); 
+
+			// recuperar parametros de main
+			int dep = 7;  // departamento
+			
+			// construir orden DE LLAMADA
+			String sql = "{ ? = call nombre_dep_jorge (?) } ";
+
+			// Preparamos la llamada
+			CallableStatement llamada = conexion.prepareCall(sql);
+			// Damos valor a los argumentos
+			
+			//Registramos el parametro de salida que esta en la posicion 1
+			llamada.registerOutParameter(1, Types.VARCHAR);
+			
+			//Doy valor al parametro de entrada (posicion 2)
+			llamada.setInt(2, dep);
+															
+			llamada.execute(); // ejecutar el procedimiento
+			
+			//Mostrar parametro de salida
+			System.out.println("Número de departamento: "+dep+" Nombre: "
+			+llamada.getString(1));//mostrar primer argumento
+			
+			llamada.close();
+			conexion.close();
+		} catch (ClassNotFoundException cn) {
+			cn.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
