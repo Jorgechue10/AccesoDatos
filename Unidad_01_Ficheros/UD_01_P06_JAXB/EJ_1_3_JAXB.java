de6fe55a360a1994fc46fb0f1f26958dfa86e578
Jorgechue10 <jorgechue10@gmail.com>
2018-11-23 12:48:14 +0100

    Pequeñas modificaciones

--- Unidad_01_Ficheros/UD_01_P06_JAXB/EJ_1_3_JAXB.java
+++ Unidad_01_Ficheros/UD_01_P06_JAXB/EJ_1_3_JAXB.java
@@ -13,7 +13,7 @@ import javax.xml.bind.Unmarshaller;
 
 public class EJ_1_3_JAXB {
  
-    private static final String MIARCHIVO_XML = "src\\P06_JAXB\\Ficheros\\libreria.xml";
+    private static final String MIARCHIVO_XML = "Unidad_01_Ficheros\\UD_01_P06_JAXB\\Ficheros\\libreria.xml";
  
     public static void main(String[] args) throws JAXBException, IOException {
         //Se crea la lista de libros
