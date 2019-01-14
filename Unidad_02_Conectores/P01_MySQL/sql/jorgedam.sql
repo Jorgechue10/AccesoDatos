3362d309e5c089a204606f349269e4c7405a8dec
Jorgechue10 <jorgechue10@gmail.com>
2019-01-14 15:16:08 +0100

    Se han añadido las bases de datos que utilizo

---
+++ Unidad_02_Conectores/P01_MySQL/sql/jorgedam.sql
@@ -0,0 +1,221 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 14-01-2019 a las 15:02:36
+-- Versión del servidor: 8.0.13
+-- Versión de PHP: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `jorgedam`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`jorgedam`@`%` PROCEDURE `actualizaDept` (`cod` INT(2), `localidad` VARCHAR(13))  BEGIN
+	UPDATE DEPT SET LOC=localidad WHERE DEPTNO = cod;
+	END$$
+
+CREATE DEFINER=`jorgedam`@`%` PROCEDURE `consultaDepar` (IN `NUM` INT(2), OUT `NAME` VARCHAR(14), OUT `LOCAL` VARCHAR(13))  BEGIN 
+SELECT DNAME, LOC into NAME, LOCAL from DEPT where DEPTNO=NUM;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `BONUS`
+--
+
+CREATE TABLE `BONUS` (
+  `ENAME` varchar(10) NOT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `SAL` int(11) DEFAULT NULL,
+  `COMM` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `DEPT`
+--
+
+CREATE TABLE `DEPT` (
+  `DEPTNO` int(2) NOT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `DEPT`
+--
+
+INSERT INTO `DEPT` (`DEPTNO`, `DNAME`, `LOC`) VALUES
+(1, '1', '1'),
+(2, '2', 'Tarazona'),
+(3, '3', '3'),
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `DUMMY`
+--
+
+CREATE TABLE `DUMMY` (
+  `DUMMY` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `DUMMY`
+--
+
+INSERT INTO `DUMMY` (`DUMMY`) VALUES
+(0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `EMP`
+--
+
+CREATE TABLE `EMP` (
+  `EMPNO` int(4) NOT NULL,
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `MGR` int(4) DEFAULT NULL,
+  `HIREDATE` date DEFAULT NULL,
+  `SAL` decimal(7,2) DEFAULT NULL,
+  `COMM` decimal(7,2) DEFAULT NULL,
+  `DEPTNO` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `EMP`
+--
+
+INSERT INTO `EMP` (`EMPNO`, `ENAME`, `JOB`, `MGR`, `HIREDATE`, `SAL`, `COMM`, `DEPTNO`) VALUES
+(7369, 'SMITH', 'CLERK', 7902, '1980-12-17', '800.00', NULL, 20),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', '1600.00', '300.00', 30),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', '1250.00', '500.00', 30),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', '2975.00', NULL, 20),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', '1250.00', '1400.00', 30),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', '2850.00', NULL, 30),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', '2450.00', NULL, 10),
+(7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', '3000.00', NULL, 20),
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', '5000.00', NULL, 10),
+(7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', '1500.00', '0.00', 30),
+(7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', '1100.00', NULL, 20),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', '950.00', NULL, 30),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', '3000.00', NULL, 20),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-23', '1300.00', NULL, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `SALGRADE`
+--
+
+CREATE TABLE `SALGRADE` (
+  `GRADE` int(11) NOT NULL,
+  `LOSAL` int(11) DEFAULT NULL,
+  `HISAL` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `SALGRADE`
+--
+
+INSERT INTO `SALGRADE` (`GRADE`, `LOSAL`, `HISAL`) VALUES
+(1, 700, 1200),
+(2, 1201, 1400),
+(3, 1401, 2000),
+(4, 2001, 3000),
+(5, 3001, 9999);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `USUARIO`
+--
+
+CREATE TABLE `USUARIO` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `BONUS`
+--
+ALTER TABLE `BONUS`
+  ADD PRIMARY KEY (`ENAME`);
+
+--
+-- Indices de la tabla `DEPT`
+--
+ALTER TABLE `DEPT`
+  ADD PRIMARY KEY (`DEPTNO`);
+
+--
+-- Indices de la tabla `DUMMY`
+--
+ALTER TABLE `DUMMY`
+  ADD PRIMARY KEY (`DUMMY`);
+
+--
+-- Indices de la tabla `EMP`
+--
+ALTER TABLE `EMP`
+  ADD PRIMARY KEY (`EMPNO`),
+  ADD KEY `DEPTNO` (`DEPTNO`);
+
+--
+-- Indices de la tabla `SALGRADE`
+--
+ALTER TABLE `SALGRADE`
+  ADD PRIMARY KEY (`GRADE`);
+
+--
+-- Indices de la tabla `USUARIO`
+--
+ALTER TABLE `USUARIO`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `EMP`
+--
+ALTER TABLE `EMP`
+  ADD CONSTRAINT `EMP_ibfk_1` FOREIGN KEY (`DEPTNO`) REFERENCES `DEPT` (`deptno`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Unidad_02_Conectores/P01_MySQL/sql/tubalcain.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 14-01-2019 a las 15:04:39
+-- Versión del servidor: 8.0.13
+-- Versión de PHP: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tubalcain`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `datos_dep` (`d` INT, OUT `nombre` VARCHAR(15), OUT `localidad` VARCHAR(15))  BEGIN
+	SET nombre = 'NO EXISTE';
+	SET localidad = 'NO EXISTE';
+	SELECT dnombre, loc INTO nombre, localidad FROM departamentos
+	WHERE dpto_no = d;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `datos_dep_alex` (`d` INT, OUT `nombre` VARCHAR(15), OUT `localidad` VARCHAR(15))  BEGIN
+	SET nombre = 'NO EXISTE';
+	SET localidad = 'NO EXISTE';
+	SELECT dnombre, loc INTO nombre, localidad FROM departamentos
+	WHERE dpto_no = d;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `datos_dep_jorge` (`d` INT, OUT `nombre` VARCHAR(15), OUT `localidad` VARCHAR(15))  BEGIN
+	SET nombre = 'NO EXISTE';
+	SET localidad = 'NO EXISTE';
+	SELECT dnombre, loc INTO nombre, localidad FROM departamentos
+	WHERE dpto_no = d;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `datos_dep_oscar` (`d` INT, OUT `nombre` VARCHAR(15), OUT `localidad` VARCHAR(15))  BEGIN
+	SET nombre = 'NO EXISTE';
+	SET localidad = 'NO EXISTE';
+	SELECT dnombre, loc INTO nombre, localidad FROM departamentos
+	WHERE dpto_no = d;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `datos_dep_toni` (`d` INT, OUT `nombre` VARCHAR(15), OUT `localidad` VARCHAR(15))  BEGIN
+	SET nombre = 'NO EXISTE';
+	SET localidad = 'NO EXISTE';
+	SELECT dnombre, loc INTO nombre, localidad FROM departamentos
+	WHERE dpto_no = d;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `subida_sal_alex` (`d` INT, `subida` INT)  BEGIN
+UPDATE empleados SET salario = salario + subida WHERE dpto_no = d;
+COMMIT;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `subida_sal_jorge` (`d` INT, `subida` INT)  BEGIN
+UPDATE empleados SET salario = salario + subida WHERE dpto_no = d;
+COMMIT;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `subida_sal_oscar` (`d` INT, `subida` INT)  BEGIN
+UPDATE empleados SET salario = salario + subida WHERE dpto_no = d;
+COMMIT;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` PROCEDURE `subida_sal_toni` (`d` INT, `subida` INT)  BEGIN
+UPDATE empleados SET salario = salario + subida WHERE dpto_no = d;
+COMMIT;
+END$$
+
+--
+-- Funciones
+--
+CREATE DEFINER=`tubalcain_dam`@`%` FUNCTION `nombre_dep_alex` (`d` INT) RETURNS VARCHAR(15) CHARSET utf8mb4 BEGIN
+	DECLARE nombre VARCHAR(15);
+	SET nombre = 'NO EXISTE';
+	SELECT dnombre INTO nombre FROM departamentos
+	WHERE dpto_no = d;
+	RETURN NOMBRE;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` FUNCTION `nombre_dep_fran` (`d` INT) RETURNS VARCHAR(15) CHARSET utf8mb4 BEGIN
+	DECLARE nombre VARCHAR(15);
+	SET nombre = 'NO EXISTE';
+	SELECT dnombre INTO nombre FROM departamentos
+	WHERE dpto_no = d;
+	RETURN NOMBRE;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` FUNCTION `nombre_dep_jorge` (`d` INT) RETURNS VARCHAR(15) CHARSET utf8mb4 BEGIN
+	DECLARE nombre VARCHAR(15);
+	SET nombre = 'NO EXISTE';
+	SELECT dnombre INTO nombre FROM departamentos
+	WHERE dpto_no = d;
+	RETURN NOMBRE;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` FUNCTION `nombre_dep_oscar` (`d` INT) RETURNS VARCHAR(15) CHARSET utf8mb4 BEGIN
+	DECLARE nombre VARCHAR(15);
+	SET nombre = 'NO EXISTE';
+	SELECT dnombre INTO nombre FROM departamentos
+	WHERE dpto_no = d;
+	RETURN NOMBRE;
+END$$
+
+CREATE DEFINER=`tubalcain_dam`@`%` FUNCTION `nombre_dep_toni` (`d` INT) RETURNS VARCHAR(15) CHARSET utf8mb4 BEGIN
+	DECLARE nombre VARCHAR(15);
+	SET nombre = 'NO EXISTE';
+	SELECT dnombre INTO nombre FROM departamentos
+	WHERE dpto_no = d;
+	RETURN NOMBRE;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamentos`
+--
+
+CREATE TABLE `departamentos` (
+  `dnombre` varchar(15) NOT NULL,
+  `dpto_no` int(11) NOT NULL,
+  `loc` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `departamentos`
+--
+
+INSERT INTO `departamentos` (`dnombre`, `dpto_no`, `loc`) VALUES
+('Taller', 87, 'Monteagudo'),
+('informatica', 10, 'tarazona'),
+('Marketing', 15, 'Malon'),
+('RRHH', 7, 'Novallas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `dni` varchar(9) NOT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  `apellidos` varchar(25) DEFAULT NULL,
+  `salario` int(11) DEFAULT NULL,
+  `dpto_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`dni`, `nombre`, `apellidos`, `salario`, `dpto_no`) VALUES
+('0000000A', 'Felipe Juan', 'de Borbon', 1100, 5),
+('12345678L', 'Armando', 'Bronca Segura', -897900, 0),
+('69696969', 'Alfredo', 'Reyes', 1000600, 3),
+('74693189K', 'pepe', 'el cejas', 1408010250, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `REGISTRATION`
+--
+
+CREATE TABLE `REGISTRATION` (
+  `id` int(11) NOT NULL,
+  `first` varchar(255) DEFAULT NULL,
+  `last` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `REGISTRATION`
+--
+
+INSERT INTO `REGISTRATION` (`id`, `first`, `last`, `age`) VALUES
+(1, 'Benito', 'Camela', 25);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `USUARIO`
+--
+
+CREATE TABLE `USUARIO` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `USUARIO`
+--
+
+INSERT INTO `USUARIO` (`id`, `nombre`, `apellido`, `edad`) VALUES
+(1, 'Benito', 'Camela', 25),
+(2, 'Joseba', 'Jartangas', 69),
+(4, 'Jony', 'Melavo', 25),
+(7, 'Aquiles', 'Castro', 22),
+(8, 'Dolores', 'Fuertes de Barriga', 41),
+(17, 'Armando', 'Guerra', 45),
+(24, 'Tomas', 'Sardinas', 50),
+(37, 'Alberto', 'Cadiscos', 37),
+(40, 'Aitor', 'Tazo', 28),
+(47, 'Esther', 'Colero del Pueblo', 57),
+(54, 'Armando', 'Juerga', 71),
+(57, 'Leandro ', 'Gado', 25),
+(79, 'Pere', 'Gil', 35),
+(90, 'Castro', 'Mascota', 41),
+(100, 'Aitor', 'Tilla', 50),
+(129, 'Tomas', 'Albino', 26),
+(625, 'Tomas', 'Turbado', 100);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `REGISTRATION`
+--
+ALTER TABLE `REGISTRATION`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `USUARIO`
+--
+ALTER TABLE `USUARIO`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
